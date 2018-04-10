05b5f1511f4e8448ab7a2cab9a92606dc692b812
wangbin <wbwuxi@gmail.com>
2018-04-10 23:51:27 +0800

    add two mm files

---
+++ notes/吃掉那只青蛙.mm
@@ -0,0 +1,57 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node BACKGROUND_COLOR="#ffffff" COLOR="#cc0033" CREATED="1523065898726" HGAP="27" ID="ID_1922978760" MODIFIED="1523373369768" STYLE="bubble" TEXT="&#x5403;&#x6389;&#x90a3;&#x53ea;&#x9752;&#x86d9;">
+<edge COLOR="#ffffff" STYLE="linear" WIDTH="2"/>
+<font NAME="Source Han Serif CN" SIZE="20"/>
+<node BACKGROUND_COLOR="#ffffff" COLOR="#000000" CREATED="1523088521539" HGAP="94" ID="ID_1584749117" MODIFIED="1523089344142" POSITION="left" STYLE="bubble" TEXT="&#x524d;&#x8a00;" VSHIFT="-5">
+<edge COLOR="#000000" STYLE="linear" WIDTH="2"/>
+<font BOLD="true" NAME="Source Han Serif CN" SIZE="18"/>
+<node BACKGROUND_COLOR="#ffffff" COLOR="#000000" CREATED="1523088596649" HGAP="187" ID="ID_1337920113" MODIFIED="1523089197736" STYLE="bubble" TEXT="&#x5411;&#x6210;&#x529f;&#x4eba;&#x58eb;&#x5b66;&#x4e60;" VSHIFT="-19">
+<edge COLOR="#cc0033" STYLE="bezier" WIDTH="1"/>
+<font NAME="Source Han Serif CN" SIZE="16"/>
+</node>
+<node BACKGROUND_COLOR="#ffffff" COLOR="#000000" CREATED="1523088691413" HGAP="46" ID="ID_1067247423" MODIFIED="1523089201356" STYLE="bubble" TEXT="&#x7b80;&#x5355;&#x7684;&#x771f;&#x7406;" VSHIFT="4">
+<edge COLOR="#cc0033" STYLE="bezier" WIDTH="1"/>
+<font NAME="Source Han Serif CN" SIZE="16"/>
+<node BACKGROUND_COLOR="#ffffff" COLOR="#000000" CREATED="1523088705368" HGAP="33" ID="ID_1434066673" MODIFIED="1523089205929" STYLE="fork" TEXT="&#x96c6;&#x4e2d;&#x7cbe;&#x529b;&#x53bb;&#x5b8c;&#x6210;&#x6700;&#x91cd;&#x8981;&#x7684;&#x4e8b;&#x60c5;&#xff0c;&#x5f7b;&#x5e95;&#x5730;&#x5b8c;&#x6210;&#x5b83;&#x4eec;&#xff0c;&#x5e76;&#x628a;&#x5b83;&#x4eec;&#x505a;&#x597d;&#x3002;&#xa;&#x8fd9;&#x662f;&#x4eba;&#x53d6;&#x5f97;&#x6210;&#x529f;&#xff0c;&#x83b7;&#x53d6;&#x5e78;&#x798f;&#x611f;&#x7684;&#x5173;&#x952e;&#x6240;&#x5728;&#x3002;" VSHIFT="1">
+<edge COLOR="#cc0033" STYLE="bezier" WIDTH="1"/>
+<font NAME="Source Han Serif CN" SIZE="16"/>
+</node>
+</node>
+</node>
+<node BACKGROUND_COLOR="#ffffff" COLOR="#000000" CREATED="1523086722294" HGAP="91" ID="ID_229482610" MODIFIED="1523089097141" POSITION="left" STYLE="bubble" TEXT="&#x5f15;&#x8a00;" VSHIFT="42">
+<edge COLOR="#000000" STYLE="linear" WIDTH="2"/>
+<font BOLD="true" NAME="Source Han Serif CN" SIZE="18"/>
+<node BACKGROUND_COLOR="#ffffff" COLOR="#000000" CREATED="1523087272855" HGAP="43" ID="ID_1805892032" MODIFIED="1523089176397" STYLE="bubble" TEXT="&#x5403;&#x9752;&#x86d9;&#x5b9a;&#x5f8b;" VSHIFT="1">
+<edge COLOR="#cc0033" STYLE="bezier" WIDTH="1"/>
+<font NAME="Source Han Serif CN" SIZE="16"/>
+<node BACKGROUND_COLOR="#ffffff" COLOR="#000000" CREATED="1523087486450" HGAP="60" ID="ID_1402135785" MODIFIED="1523088141643" STYLE="fork" TEXT="&#x5982;&#x679c;&#x4f60;&#x5fc5;&#x987b;&#x5403;&#x6389;&#x4e24;&#x53ea;&#x9752;&#x86d9;&#xff0c;&#x90a3;&#x4e48;&#x8981;&#x5148;&#x5403;&#x90a3;&#x53ea;&#x957f;&#x5f97;&#x66f4;&#x4e11;&#x964b;&#x7684;&#x3002;" VSHIFT="-21">
+<edge COLOR="#cc0033" STYLE="bezier" WIDTH="1"/>
+<font NAME="Source Han Serif CN" SIZE="16"/>
+</node>
+<node BACKGROUND_COLOR="#ffffff" COLOR="#000000" CREATED="1523087698719" HGAP="60" ID="ID_934070244" MODIFIED="1523088164680" STYLE="fork" TEXT="&#x5982;&#x679c;&#x4f60;&#x8981;&#x5b8c;&#x5168;&#x5403;&#x6389;&#x4e00;&#x53ea;&#x6d3b;&#x9752;&#x86d9;&#xff0c;&#x5374;&#x53ea;&#x662f;&#x4e00;&#x76f4;&#x5750;&#x5728;&#x90a3;&#x91cc;&#x76ef;&#x7740;&#x5b83;&#x770b;&#xff0c;&#xa;&#x770b;&#x5f97;&#x518d;&#x4e45;&#x4e5f;&#x65e0;&#x6d4e;&#x4e8e;&#x4e8b;&#x3002;" VSHIFT="1">
+<edge COLOR="#cc0033" STYLE="bezier" WIDTH="1"/>
+<font NAME="Source Han Serif CN" SIZE="16"/>
+</node>
+</node>
+<node BACKGROUND_COLOR="#ffffff" COLOR="#000000" CREATED="1523088946919" HGAP="63" ID="ID_789455607" MODIFIED="1523089021531" STYLE="fork" TEXT="&#x4f60;&#x60f3;&#x50cf;&#x5e76;&#x770b;&#x5230;&#x81ea;&#x5df1;&#x662f;&#x4ec0;&#x4e48;&#x6837;&#x7684;&#x4eba;&#xff0c;&#x4f60;&#x5c31;&#x4f1a;&#x6210;&#x4e3a;&#x4ec0;&#x4e48;&#x6837;&#x7684;&#x4eba;" VSHIFT="33">
+<edge COLOR="#cc0033" STYLE="bezier" WIDTH="1"/>
+<font NAME="Source Han Serif CN" SIZE="16"/>
+</node>
+</node>
+<node BACKGROUND_COLOR="#ffffff" COLOR="#000000" CREATED="1523373386072" HGAP="44" ID="ID_1601852225" MODIFIED="1523373453557" POSITION="right" STYLE="bubble" TEXT="21&#x6761;&#x65f6;&#x95f4;&#x7ba1;&#x7406;&#x6cd5;&#x5219;" VSHIFT="-1">
+<edge COLOR="#000000" STYLE="linear" WIDTH="2"/>
+<font BOLD="true" NAME="Source Han Serif CN" SIZE="18"/>
+<node BACKGROUND_COLOR="#ffffff" COLOR="#000000" CREATED="1523373458557" HGAP="25" ID="ID_1044033391" MODIFIED="1523373772976" STYLE="fork" TEXT="1-&#x660e;&#x786e;&#x76ee;&#x6807;" VSHIFT="-174">
+<edge COLOR="#cc0033" STYLE="bezier" WIDTH="1"/>
+<font NAME="Source Han Serif CN" SIZE="16"/>
+<node CREATED="1523373818279" HGAP="34" ID="ID_1027200439" MODIFIED="1523373995421" TEXT="&#x62e5;&#x6709;&#x660e;&#x786e;&#x7684;&#x76ee;&#x6807;&#x6216;&#x8bb8;&#x662f;&#x4e2a;&#x4eba;&#x751f;&#x4ea7;&#x529b;&#x9886;&#x57df;&#x6700;&#x91cd;&#x8981;&#x7684;&#x6982;&#x5ff5;&#x4e86;&#x3002;" VSHIFT="-31"/>
+<node CREATED="1523374015054" ID="ID_255299448" MODIFIED="1523374193328" STYLE="bubble" TEXT="&#x5403;&#x9752;&#x86d9;&#x5b9a;&#x5f8b;">
+<font NAME="Source Han Serif CN" SIZE="16"/>
+<node CREATED="1523373597968" HGAP="39" ID="ID_1104611647" MODIFIED="1523374311757" STYLE="fork" TEXT="&#x6700;&#x7cdf;&#x7cd5;&#x7684;&#x5229;&#x7528;&#x65f6;&#x95f4;&#x7684;&#x60c5;&#x5f62;&#x4e4b;&#x4e00;&#xff0c;&#x662f;&#x628a;&#x6839;&#x672c;&#x4e0d;&#x9700;&#x8981;&#x505a;&#x7684;&#x4e8b;&#x60c5;&#x505a;&#x5f97;&#x5f88;&#x5b8c;&#x7f8e;&#x3002;" VSHIFT="-18"/>
+<node CREATED="1523374113410" HGAP="48" ID="ID_1189471955" MODIFIED="1523374315614" STYLE="fork" TEXT="&#x628a;&#x76ee;&#x6807;&#x5199;&#x5728;&#x7eb8;&#x4e0a;" VSHIFT="-8"/>
+</node>
+</node>
+</node>
+</node>
+</map>
---
+++ notes/计划.mm
@@ -0,0 +1,31 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1523373281208" ID="ID_381540093" MODIFIED="1523374356306" TEXT="&#x76ee;&#x6807;">
+<font NAME="Source Han Serif CN" SIZE="20"/>
+<node COLOR="#006633" CREATED="1523374362594" HGAP="73" ID="ID_1547256710" MODIFIED="1523374682676" POSITION="right" TEXT="&#x8d22;&#x52a1;&#x81ea;&#x7531;" VSHIFT="-10">
+<edge COLOR="#000000" STYLE="linear" WIDTH="2"/>
+<font BOLD="true" NAME="Source Han Serif CN" SIZE="18"/>
+<node BACKGROUND_COLOR="#ffffff" COLOR="#666666" CREATED="1523374576822" HGAP="39" ID="ID_298607457" MODIFIED="1523375211538" STYLE="bubble" TEXT="5&#x5e74;&#x65f6;&#x95f4;" VSHIFT="1">
+<edge COLOR="#999999" STYLE="bezier" WIDTH="thin"/>
+<font NAME="Source Han Serif CN" SIZE="16"/>
+</node>
+<node BACKGROUND_COLOR="#ffffff" COLOR="#000000" CREATED="1523374687389" HGAP="83" ID="ID_1684964041" MODIFIED="1523375351360" STYLE="bubble" TEXT="&#x6709;&#x81ea;&#x5df1;&#x7684;&#x56e2;&#x961f;&#x6216;&#x516c;&#x53f8;" VSHIFT="-3">
+<edge COLOR="#cc0033" STYLE="bezier" WIDTH="2"/>
+<font NAME="Source Han Serif CN" SIZE="18"/>
+<node CREATED="1523374938015" HGAP="60" ID="ID_51141313" MODIFIED="1523375079109" TEXT="&#x5bfb;&#x6c42;&#x4eba;&#x624d;" VSHIFT="-42">
+<font NAME="Source Han Serif CN" SIZE="18"/>
+<node COLOR="#666666" CREATED="1523375180078" ID="ID_1090157538" MODIFIED="1523375251375" TEXT="2&#x5e74;&#x65f6;&#x95f4;" VSHIFT="-77">
+<edge COLOR="#999999" WIDTH="thin"/>
+<font NAME="Source Han Serif CN" SIZE="16"/>
+</node>
+</node>
+<node CREATED="1523375057745" HGAP="58" ID="ID_214028775" MODIFIED="1523375302439" TEXT="&#x79ef;&#x7d2f;&#x4e00;&#x5b9a;&#x7684;&#x4e1a;&#x52a1;&#x89c4;&#x6a21;" VSHIFT="1">
+<font NAME="Source Han Serif CN" SIZE="18"/>
+</node>
+<node CREATED="1523375142326" HGAP="56" ID="ID_103594719" MODIFIED="1523375160881" TEXT="&#x6210;&#x7acb;&#x516c;&#x53f8;&#x6216;&#x4e13;&#x4e1a;&#x56e2;&#x961f;" VSHIFT="31">
+<font NAME="Source Han Serif CN" SIZE="18"/>
+</node>
+</node>
+</node>
+</node>
+</map>
