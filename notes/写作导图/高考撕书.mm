7a54af3636a6875ab3ade8429e2d74bd84e03a75
wangbin <wbwuxi@gmail.com>
2018-06-18 23:02:25 +0800

    update

---
+++ notes/写作导图/高考撕书.mm
@@ -0,0 +1,26 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1529292619728" ID="ID_923518375" MODIFIED="1529294050252" TEXT="&#x9ad8;&#x8003;&#x6495;&#x4e66;">
+<font BOLD="true" NAME="Source Han Serif CN" SIZE="18"/>
+<node BACKGROUND_COLOR="#ffffff" COLOR="#000000" CREATED="1529292636327" HGAP="62" ID="ID_1324972213" MODIFIED="1529292701101" POSITION="right" STYLE="bubble" TEXT="&#x7eb8;&#x98de;&#x673a;" VSHIFT="-60">
+<edge COLOR="#000000" STYLE="bezier" WIDTH="2"/>
+<font BOLD="true" NAME="Source Han Serif CN" SIZE="16"/>
+</node>
+<node BACKGROUND_COLOR="#ffffff" COLOR="#000000" CREATED="1529292733724" HGAP="62" ID="ID_1145925218" MODIFIED="1529292747592" POSITION="right" STYLE="bubble" TEXT="&#x72ec;&#x7acb;&#x601d;&#x8003;" VSHIFT="3">
+<edge COLOR="#000000" STYLE="bezier" WIDTH="2"/>
+<font BOLD="true" NAME="Source Han Serif CN" SIZE="16"/>
+</node>
+<node BACKGROUND_COLOR="#ffffff" COLOR="#000000" CREATED="1529292793055" HGAP="49" ID="ID_1018665824" MODIFIED="1529292865017" POSITION="left" STYLE="bubble" TEXT="&#x60c5;&#x611f;&#x7684;&#x6e32;&#x6cc4;">
+<edge COLOR="#000000" STYLE="bezier" WIDTH="2"/>
+<font BOLD="true" NAME="Source Han Serif CN" SIZE="16"/>
+</node>
+<node BACKGROUND_COLOR="#ffffff" COLOR="#000000" CREATED="1529292892418" HGAP="54" ID="ID_1839157646" MODIFIED="1529293303991" POSITION="left" STYLE="bubble" TEXT="&#x66b4;&#x529b;&#x3001;&#x72c2;&#x6b22;&#x3001;&#x653e;&#x7eb5;" VSHIFT="35">
+<edge COLOR="#000000" STYLE="bezier" WIDTH="2"/>
+<font BOLD="true" NAME="Source Han Serif CN" SIZE="16"/>
+</node>
+<node BACKGROUND_COLOR="#ffffff" COLOR="#000000" CREATED="1529294018189" HGAP="61" ID="ID_5263263" MODIFIED="1529294036537" POSITION="right" STYLE="bubble" TEXT="&#x5c0a;&#x91cd;&#x3001;&#x5bbd;&#x5bb9;" VSHIFT="25">
+<edge COLOR="#000000" STYLE="bezier" WIDTH="2"/>
+<font BOLD="true" NAME="Source Han Serif CN" SIZE="16"/>
+</node>
+</node>
+</map>
