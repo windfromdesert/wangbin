b043d968243597e196c097c9f6da51a63ef80222
wangbin <wbwuxi@gmail.com>
2019-01-03 17:42:47 +0800

    update

---
+++ notes/2019年非车险规划.mm
@@ -0,0 +1,37 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1546505999504" ID="ID_196168991" MODIFIED="1546506026395" TEXT="2019&#x5e74;&#x975e;&#x8f66;&#x9669;&#x89c4;&#x5212;">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="18"/>
+<node CREATED="1546506037992" HGAP="18" ID="ID_761101996" MODIFIED="1546506406011" POSITION="right" STYLE="bubble" TEXT="&#x6211;" VSHIFT="-92">
+<font NAME="&#x9ed1;&#x4f53;" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1546506155022" HGAP="57" ID="ID_708014761" MODIFIED="1546506187423" POSITION="left" TEXT="&#x4eba;&#x624d;&#x5f15;&#x8fdb;" VSHIFT="-67">
+<edge COLOR="#0033ff" WIDTH="4"/>
+<font NAME="&#x9ed1;&#x4f53;" SIZE="16"/>
+</node>
+<node COLOR="#0033ff" CREATED="1546506207277" HGAP="110" ID="ID_840324363" MODIFIED="1546506419227" POSITION="right" TEXT="&#x677f;&#x5757;&#x4e1a;&#x52a1;&#xff0c;&#x7ecf;&#x4ee3;&#x516c;&#x53f8;" VSHIFT="-43">
+<edge COLOR="#0033ff" WIDTH="4"/>
+<font NAME="&#x9ed1;&#x4f53;" SIZE="16"/>
+</node>
+<node COLOR="#0033ff" CREATED="1546506272204" HGAP="71" ID="ID_162230680" MODIFIED="1546506387268" POSITION="left" TEXT="&#x8bc9;&#x8d23;&#x9669;" VSHIFT="-28">
+<edge COLOR="#0033ff" WIDTH="4"/>
+<font NAME="&#x9ed1;&#x4f53;" SIZE="16"/>
+</node>
+<node COLOR="#0033ff" CREATED="1546506300740" HGAP="110" ID="ID_120212087" MODIFIED="1546506413931" POSITION="right" TEXT="&#x5171;&#x4fdd;&#x4e1a;&#x52a1;" VSHIFT="-32">
+<edge COLOR="#0033ff" WIDTH="4"/>
+<font NAME="&#x9ed1;&#x4f53;" SIZE="16"/>
+</node>
+<node COLOR="#0033ff" CREATED="1546506305940" HGAP="76" ID="ID_86728365" MODIFIED="1546506389899" POSITION="left" TEXT="&#x57f9;&#x8bad;&#x8ba1;&#x5212;" VSHIFT="-4">
+<edge COLOR="#0033ff" WIDTH="4"/>
+<font NAME="&#x9ed1;&#x4f53;" SIZE="16"/>
+</node>
+<node COLOR="#0033ff" CREATED="1546506338636" HGAP="118" ID="ID_1828150639" MODIFIED="1546506416667" POSITION="right" TEXT="&#x5957;&#x9910;&#x5f0f;&#x65b9;&#x6848;&#x53d1;&#x5c55;" VSHIFT="-3">
+<edge COLOR="#0033ff" WIDTH="4"/>
+<font NAME="&#x9ed1;&#x4f53;" SIZE="16"/>
+</node>
+<node COLOR="#0033ff" CREATED="1546506367884" HGAP="46" ID="ID_1619601949" MODIFIED="1546506392131" POSITION="left" TEXT="&#x4e92;&#x8054;&#x7f51;&#x3001;&#x4e92;&#x91d1;&#x4e1a;&#x52a1;" VSHIFT="32">
+<edge COLOR="#0033ff" WIDTH="4"/>
+<font NAME="&#x9ed1;&#x4f53;" SIZE="16"/>
+</node>
+</node>
+</map>
