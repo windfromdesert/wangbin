b9403fb7be5930c91f1714aa02753475f50206ad
wangbin <wbwuxi@gmail.com>
2019-01-13 22:26:43 +0800

    add 记忆导图

---
+++ notes/思维导图笔记/记忆导图.mm
@@ -0,0 +1,20 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1547388475184" ID="2c7i4qofi8tiaavse1o2aj5mt8" MODIFIED="1547389214092" STYLE="bubble" TEXT="&#x8bb0;&#x5fc6;&#x5bfc;&#x56fe;">
+<font NAME="Source Han Serif CN" SIZE="18"/>
+<node COLOR="#0033ff" CREATED="1547388475184" ID="2k80b1cqqpi9c20ok5cjuf76ip" MODIFIED="1547389267043" POSITION="right" TEXT="&#x9996;&#x56e0;&#x6548;&#x5e94;-Primacy Effect&#xa;&#x8fd1;&#x56e0;&#x6548;&#x5e94;-Recency Effect&#xa;&#x51af;-&#x96f7;&#x65af;&#x6258;&#x592b;&#x6548;&#x5e94;-Von Restorff Effect&#xa;&#x8054;&#x60f3;&#x6548;&#x5e94;-Association Effect&#xa;&#x7406;&#x89e3;&#x548c;&#x8bef;&#x89e3;&#x6548;&#x5e94;-Understanding and Misunderstanding Effect&#xa;&#x5174;&#x8da3;&#x6548;&#x5e94;-Interest Effect&#xa;&#x610f;&#x4e49;&#x6548;&#x5e94;-Effect of Meaning" VSHIFT="-113">
+<edge COLOR="#0033ff" WIDTH="2"/>
+</node>
+<node COLOR="#0033ff" CREATED="1547388475185" ID="6ameobg4pkgd96a5sir7ce9s3l" MODIFIED="1547389278183" POSITION="right" TEXT="&#x751f;&#x6d3b;&#x7684;&#x65f6;&#x4ee3;">
+<edge COLOR="#0033ff" WIDTH="2"/>
+<node COLOR="#000000" CREATED="1547388475185" HGAP="53" ID="1lf9gfg4simvf6vf7pdj592l3t" MODIFIED="1547389284605" TEXT="&#x571f;&#x8457;&#x3001;&#x519c;&#x4e1a;&#x3001;&#x5de5;&#x4e1a;&#x3001;&#x4fe1;&#x606f;&#x3001;&#x77e5;&#x8bc6;&#x3001;&#x667a;&#x529b;" VSHIFT="-9">
+<edge COLOR="#000000" WIDTH="2"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#000000" CREATED="1547388475185" HGAP="52" ID="1j7fvahti0tl1spvnu6ekejk15" MODIFIED="1547389282129" TEXT="&#x5bf9;&#x8bb0;&#x5fc6;&#x7684;&#x7ec4;&#x6210;&#x5143;&#x7d20;&#x548c;&#x6210;&#x5206;&#x8fdb;&#x884c;&#x79d1;&#x5b66;&#x89e3;&#x91ca;&#x548c;&#x63a2;&#x7d22;&#xa;&#x667a;&#x6167;&#x5730;&#x601d;&#x8003;&#x3001;&#x4f7f;&#x7528;&#x548c;&#x7ba1;&#x7406;&#x8bb0;&#x5fc6;&#xa;&#x638c;&#x63a7;&#x8bb0;&#x5fc6;&#xa;&#x63d0;&#x5347;&#x601d;&#x8003;&#x54c1;&#x8d28;&#xff0c;&#x5e2e;&#x52a9;&#x4f60;&#x5f00;&#x8f9f;&#x7ba1;&#x7406;&#x81ea;&#x8eab;&#x548c;&#x751f;&#x6d3b;&#x7684;&#x65b0;&#x53ef;&#x80fd;&#xa;&#x5b9e;&#x73b0;&#x53ef;&#x884c;&#x7684;&#x68a6;&#x60f3;" VSHIFT="21">
+<edge COLOR="#000000" WIDTH="2"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+</map>
